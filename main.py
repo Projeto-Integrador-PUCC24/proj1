1ebc9e6134f0ffefbee6d4f2facdc6ecd9826c82
davMac606 <cl201238@g.unicamp.br>
2024-04-12 08:22:55 -0300

    i didn't have enough brain to acutally think of commits so i'm commiting it all now.

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ def cls():
     os.system("cls" if os.name == "nt" else "clear")
 
 def menu():
-    menu = int(input("\n\nChoose an option!\n[1]Add a product.\n[2]Remove a product.\n[3]Update a product\n[4]List all products\n[5]Search for a product\n[6]Exit.\n"))
-    match menu:
-      case 1:
-        menumethods.prodAdding()
-      case 2:
-        menumethods.prodRemoving()
-      case 3:
-        menumethods.prodUpdating()
-      case 4:
-        menumethods.prodListing()
-      case 5:
-        menumethods.prodSearching()
-      case 6:
-        print("\n\nGoodbye!")
-      case _:
-        print("\n\nInvalid option!")
+    while True:
+      try:
+        menu = int(input("\n\nChoose an option!\n[1]Add a product.\n[2]Remove a product.\n[3]Update a product\n[4]List all products\n[5]Search for a product\n[6]Exit.\n"))
+        match menu:
+          case 1:
+            menumethods.prodAdding()
+          
+          case 2:
+            menumethods.prodRemoving()
+          case 3:
+            menumethods.prodUpdating()
+          case 4:
+            menumethods.prodListing()
+          case 5:
+            menumethods.prodSearching()
+          case 6:
+            print("\n\nGoodbye!")
+          case _:
+            print("\n\nInvalid option!")
+      except ValueError:
+        print("\n\nInvalid value! Please try again.")
+        time.sleep(1)
+        cls()
 
 if __name__ == "__main__":
   cls()
