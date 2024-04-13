bbba95ad6341b1739ba55f85e9d6883fad3dd823
davMac606 <worksender053@gmail.com>
2024-04-12 21:36:40 -0300

    Rounding numbers
    Forgot to format these.

--- menumethods.py
+++ menumethods.py
@@ -8,14 +8,7 @@ def prodAdding():
       product_id = int(product_id)
       if(product_id < 0):
         print("\n\nInvalid ID!")
-        prodAdding()
-      product_name = "Coca-Cola"
-      product_desc = "Refrigerante de cola"
-      product_cost = 36.00
-      product_cf_percent = 15
-      product_cv_percent = 5
-      product_tax_percent = 12
-      product_ml_percent = 20
+        continue
       product_name = input("Insira o nome do produto: ")
       product_desc = input("Insira a descrição do produto: ")
       product_cost = float(input("Insira o custo do produto: "))
@@ -35,9 +28,6 @@ def prodAdding():
         print("\n\Valor inválido!")
         prodAdding()
       product_ml_percent = float(input("Insira a rentabilidade/margem de lucro: "))
-      if (product_ml_percent < 0):
-        print("\n\Valor inválido!")
-        continue 
       grossIncome_percent = product_cf_percent + product_cv_percent + product_tax_percent + product_ml_percent
       grossIncome_percentCalc = (grossIncome_percent / 100) * product_cost
       division = 1 - (grossIncome_percentCalc / product_cost)
@@ -66,20 +56,20 @@ def prodAdding():
       others_percent = (others / sellingPrice) * 100
       print("=============================================")
       print("\n\nVisão detalhada: ")
-      productDetails = [["Descrição", "Valor", "%"],["Preço de Venda", f"R${sellingPrice}", 100],
-            ["Custo de aquisição", f"R${product_cost}", sellingPrice_percent ],
-            ["Receita Bruta", f"R${grossIncome}", grossIncome_percent],
-            ["Custo Fixo", f"R${product_cf}", product_cf_percent],
-            ["Comissão de Vendas", f"R${product_cv}", product_cv_percent],
-            ["Impostos", f"R${product_tax}", product_tax_percent],
-            ["Outros custos", f"R${others}", others_percent], 
-            ["Rentabilidade", f"R${product_ml}", product_ml_percent],
+      productDetails = [["Descrição", "Valor", "%"],["Preço de Venda", round(sellingPrice,2), 100],
+            ["Custo de aquisição", round(product_cost,2), sellingPrice_percent ],
+            ["Receita Bruta", round(grossIncome, 2), grossIncome_percent],
+            ["Custo Fixo", round(product_cf,2), product_cf_percent],
+            ["Comissão de Vendas", round(product_cv,2), product_cv_percent],
+            ["Impostos", round(product_tax,2), product_tax_percent],
+            ["Outros custos",round(others,2), others_percent], 
+            ["Rentabilidade", round(product_ml,2), product_ml_percent],
             ["Descrição da Margem de Lucro", product_mlDesc, product_ml_percent]
             ]
       table2 = tabulate.tabulate(productDetails,headers = "firstrow", tablefmt = "github")
       print(table2)
       print("=============================================")
-      products = [["Nome", "Preço de Venda", "Margem de Lucro"],[product_name, sellingPrice, product_ml]]
+      products = [["Nome", "Preço de Venda", "Margem de Lucro"],[product_name, round(sellingPrice,2), round(product_ml,2)]]
 
       print("\n\nVisão geral: ")
       table1 = tabulate.tabulate(products,headers = "firstrow", tablefmt = "github")
