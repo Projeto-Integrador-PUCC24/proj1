ada26fd79eab215ea83c558f9f0a15527a3c28ac
davMac606 <cl201238@g.unicamp.br>
2024-04-18 08:28:03 -0300

    Removed name and description inputs
    Name and description values deemed as useless, discarded from code.

--- menumethods.py
+++ menumethods.py
@@ -9,15 +9,11 @@ def prodAdding():
       if(product_id < 0):
         print("\n\nCódigo inválido!")
         prodAdding()
-      product_name = "Coca-Cola"
-      product_desc = "Refrigerante de cola"
       product_cost = 36.00
       product_cf_percent = 15
       product_cv_percent = 5
       product_tax_percent = 12
       product_ml_percent = 20
-      product_name = input("Insira o nome do produto: ")
-      product_desc = input("Insira a descrição do produto: ")
       product_cost = float(input("Insira o custo do produto: "))
       if (product_cost < 0):
         print("\n\Valor inválido!")
